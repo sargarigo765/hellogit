ef285b187ac51dadb6abeac7dc2ce489ec02ccae
Gyarmati Krisztián <steel765@gmail.com>
2018-06-30 23:12:04 +0200

    Hozzáadtam a youtube linket

--- HelloGit/src/net/sargarigo/hellogit/HelloGit.java
+++ HelloGit/src/net/sargarigo/hellogit/HelloGit.java
@@ -1,7 +1,10 @@
 package net.sargarigo.hellogit;
 
 public class HelloGit {
-
+/**
+ * A tutorial ez: https://www.youtube.com/watch?v=cdsMIX9gB94
+ * 
+ */
 	public static void main(String[] args) {
 		// üdvözlõ sor
 		System.out.println("Hello Git!");
